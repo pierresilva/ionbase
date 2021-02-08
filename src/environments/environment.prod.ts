33e613a8e9f3cd1ac46a610a55486bc6f80051d3
Pierre Silva <pierremichelsilva@gmail.com>
2021-02-08 09:32:27 -0500

    to publish

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     version: 'pro',
     name: 'Noah App',
-    serverUrl: '/', // 'https://ionbase-be.test',
+    serverUrl: '', // 'https://ionbase-be.test',
     secretKey: 'Zcpocr3nMl1lyvnkGT6zMSc'
 };
